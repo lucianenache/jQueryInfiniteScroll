35ba32242f9c4962f875c9c7a8e0b93f80af6959
lucianenache <enache.lucian@europe.com>
2014-10-12 23:12:30 +0200

    final commit

--- script.js
+++ script.js
@@ -1,56 +1,112 @@
+'use strict';
 
-var offset = 0;
-var limit = 15; /* enough elements to activate the scrollbar*/
+var InfiniteList = (function () {
+  var pub = {};
 
-getFakeData(0,limit,display);
+  var offset = 0;
+  var limit = 15; /* enough elements to activate the scrollbar*/
+  var serviceEndpoint = null;
+  var displayFunction = null;
 
-/* loop over the data and display it inside the page*/
-function display(i, datas){
-  console.log('called cb',datas);
-  $.each(datas, function(i, data) {
-    $.each(data, function(key,val) {
-      $('#infinite-list').append('<span>'+key+': '+val+'</span><br>' );                   
-    });
-    $('#infinite-list').append('<hr>');   
-  });   
-}
-
-/* if scrolling to the bottom -10% start already loading new stuff */
-$(document).ready(function(){
-  $("#infinite-list").scroll(function(){
-    var infiniteList = $('#infinite-list');
-
-    console.log('$(window).scrollTop()',infiniteList.scrollTop());
-    console.log('$(document).height()',$(document).height());
-    console.log('$(window).height()',infiniteList.height());
-    console.log('$(document).height()*0.05)',$(document).height()*0.1);
-    console.log('difference: ',$(document).height()-infiniteList.height());
-    
-    if(infiniteList.scrollTop() + infiniteList.innerHeight() >= infiniteList.prop('scrollHeight')){
-      offset +=limit;
-      getFakeData(offset,limit,display);
-    }
-  });
-});
-
-
-/* simulate a webservice */
+  /* simulate a webservice */
   function getFakeData(offset, limit, callback) {
     var data = [];
-    for (var i=0; i<limit; i++) {
-      var id = offset + i;
+    var i;
+    var id;
+    for (i = 0; i < limit; i++) {
+      id = offset + i;
       data.push({
         id: id,
         name: "Name " + id,
         description: "Description " + id
       });
     }
-    console.log("loaded the data",data);
-    setTimeout(function(){
-      console.log("timeout");
+    setTimeout(function () {
       callback(null, data);
-    }, 1000) /* simulate 1s delay for the service call */
-  };
+    }, 1000); /* simulate 1s delay for the service call */
+  }
 
+  /* real Http service */
+  function getRealData(offset, limit, callback, serviceEndpoint) {
+    var err = {};
+    $.ajax({
+      'url' : serviceEndpoint,
+      'type' : 'GET',
+      'data' : {
+        'offset' : offset,
+        'limit' : limit
+      },
+      'success' : function (data) {
+        callback(null, data);
+      },
+      'error' : function (data, status, error) {
+        err.push(status);
+        err.push(error);
+        callback(err, data);
+      }
+    });
+  }
 
+  /* loop over the data and display it inside the page*/
+  function display(err, datas) {
+    if (err) {
+      console.log('Something went wrong', err);
+    } else {
+      $.each(datas, function (i, data) {
+        $.each(data, function (key, val) {
+          $('#infinite-list').append('<span>' + key + ': ' + val + '</span><br>');
+        });
+        $('#infinite-list').append('<hr>');
+      });
+    }
+  }
+
+  function loadData(o, l) {
+    if (arguments.length !== 2) {
+      console.log('Usage: InfiniteList.loadData(offset, length)');
+    } else {
+      if (serviceEndpoint === 'local') {
+        getFakeData(o, l, displayFunction);
+      } else {
+        getRealData(o, l, displayFunction, serviceEndpoint);
+      }
+    }
+  }
+
+  /* setDisplay()*/
+  function setDisplay(d) {
+    if (d === null || d === undefined) {
+      displayFunction = display;
+    } else {
+      displayFunction = d;
+    }
+  }
+
+  /* setService */
+  function setService(s) {
+    if (s === null || s === undefined) {
+      serviceEndpoint = 'local';
+    } else {
+      serviceEndpoint = s;
+    }
+  }
+
+  /* when scrolling to the bottom start loading the new stuff */
+  $(document).ready(function () {
+    $("#infinite-list").scroll(function () {
+      var infiniteList = $('#infinite-list');
+      if (infiniteList.scrollTop() + infiniteList.innerHeight() >= infiniteList.prop('scrollHeight')) {
+        offset += limit;
+        loadData(offset, limit);
+      }
+    });
+  });
+
+  /* define public methods for the module */
+  pub.setDisplay = setDisplay;
+  pub.setService = setService;
+  pub.loadData = loadData;
+
+  return pub;
+}());
 
